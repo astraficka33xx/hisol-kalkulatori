104c7847e7f862fdc08e741b39b396edc1e94a6f
astraficka33xx <astraficka151@gmail.com>
2026-09-10 10:27:09 +0200

    update js/app.js

--- js/app.js
+++ js/app.js
@@ -310,7 +310,7 @@ el("modelsBtn").addEventListener("click", () => {
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js").catch(() => {});
+    navigator.serviceWorker.register("sw.js").catch(() => {});
   });
 }
 
